4ed6c3a8d05aa9735ade54da33a14bf1e02cd95b
badu51 <emmanuelprata@gmail.com>
2024-06-18 22:25:18 -0300

    projeto novo

---
+++ cities-api/src/test/java/com/github/badu51/cities_api/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.badu51.cities_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
